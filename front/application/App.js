201addb3e9d07aae003c9275fd9e3c2d5f7cea9e
murmilad <murmilad@gmail.com>
2023-07-30 13:59:33 +0300

    add deeplink

--- front/application/App.js
+++ front/application/App.js
@@ -8,15 +8,21 @@ import './i18n';
 import {store, persistor} from './redux/store'
 
 import { NavigationContainer } from '@react-navigation/native';
+import * as Linking from 'expo-linking';
 
+const prefix = Linking.createURL('/');
 
 
 export default function App() {
+  const linking = {
+    prefixes: [prefix],
+  };
+
   return (
     <Provider store={store}>
     <StatusBar />
       <PersistGate loading={null} persistor={persistor}>
-        <NavigationContainer>
+        <NavigationContainer linking={linking}>
           <EntryNavigation />
         </NavigationContainer>
       </PersistGate>
